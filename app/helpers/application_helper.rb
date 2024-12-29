8c49610a969171cfaa9113dc17a49d1ba2f59bfc
Jack Willis <jwillis@sprocketsecurity.com>
2024-12-28 18:54:10 -0600

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
+  SITE_TITLE = 'Princess Purple Pink'.freeze
+
   def site_title
-    'Princess Purple Pink'
+    SITE_TITLE
+  end
+
+  def page_title
+    # Return formatted title with subtitle if present, otherwise return title or default site title.
+    subtitle = @subtitle.presence || content_for(:subtitle)
+    return "#{subtitle} - #{SITE_TITLE}" if subtitle.present?
+  
+    return @title.presence || content_for(:title).presence || SITE_TITLE
+  end  
+
+  def page_tagline
+    @tagline || content_for(:tagline) || content_for(:default_tagline)
   end
 
   def navigation_items
