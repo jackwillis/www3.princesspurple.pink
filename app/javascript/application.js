2eda097f6c118f8b2b9c354eb86fd465c571b465
Jackson Willis <jack@attac.us>
2025-01-01 20:57:13 -0600

    load trix only on pages where it's needed
    it's like 500 kB

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import 'trix';
-import '@rails/actiontext';
 import '@hotwired/turbo-rails';
-
 import 'controllers';
+
+document.addEventListener('turbo:load', () => {
+  // If the page has a Trix editor, import Trix & ActionText
+  if (document.querySelector('trix-editor')) {
+    import('trix');
+    import('@rails/actiontext');
+  }
+});
