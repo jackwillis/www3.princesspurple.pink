e150cb3ab771a5e2421956d50499fd45a413ea77
Jackson Willis <jack@attac.us>
2025-01-01 20:37:43 -0600

    install draper gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.news_article
-
-    @articles = @articles.to_a
-    while 0 < @articles.size && @articles.size < 10
-      @articles.concat(@articles)
-    end
+    @articles = Article.news_article.decorate
   end
 end
